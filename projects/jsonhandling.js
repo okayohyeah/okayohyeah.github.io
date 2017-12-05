c15f38425838ff2832284a04b482f49b1fad328a
Katherine Oh <ohkathy@gmail.com>
2017-12-05 12:11:32 -0800

    returned avatars and a links

--- projects/jsonhandling.js
+++ projects/jsonhandling.js
@@ -19,7 +19,8 @@ function getUsername() {
 		console.log(obj);
 
 		// otherwise username exists and render the following
-		document.getElementById("username").setAttribute("href", "http://www.github.com/" + obj.login);
+		document.getElementById("pic").src = obj.avatar_url;
+		document.getElementById("a_pic").setAttribute("href", "http://www.github.com/" + obj.login);
 		document.getElementById("username").innerText = "@" +obj.login;
 		
 		if (obj.name !== null) {
@@ -27,9 +28,8 @@ function getUsername() {
 		} else {
 			document.getElementById("name").innerText = "A user has no name";
 		}
-		
-		document.getElementById("pic").src = obj.avatar_url;
 
+		// creates location icon
 		document.getElementById("fill-icon").setAttribute("d", "M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z");
 
 		if (obj.location !== null) {
@@ -71,9 +71,6 @@ function getFollowers(url) {
 		return followers_obj.json();
 	})
 	.then(function(obj) {
-
-		//check obj returned
-		console.log(obj);
 	})
 }
 
@@ -84,14 +81,10 @@ function showFollowers(url) {
 	})
 	.then(function(obj) {
 		obj.forEach(function(item,index) {
-				var avatar = document.createElement("img");
-				avatar.setAttribute("src", obj[index].avatar_url);
-				avatar.setAttribute("id", "f-icon");
-				avatar.setAttribute("width", "6%");
-				avatar.setAttribute("height", "6%");
-				avatar.setAttribute("alt", "Avatar");
-				avatar.setAttribute("hspace", "1%");
-				document.getElementById("followers_avatar").appendChild(avatar);
+			var span = document.createElement("span");
+			span.innerHTML = '<a href="http://www.github.com/'+ obj[index].login + '" target="_blank">' + '<img src="' + obj[index].avatar_url + '" id="f-icon" width=6% height=6% alt="follower avatar" hspace=1%/>';
+			document.getElementById("followers_avatar").appendChild(span);
+
 		});
 	})
 }
@@ -102,28 +95,18 @@ function getFollowing(url) {
 		return following_obj.json();
 	})
 	.then(function(obj) {
-
-		// check for obj returned
-		console.log(obj);
 	})
 }
 
 function showFollowing(url) {
 	fetch(url)
 	.then(function(show_obj) {
-		// check for obj returned
-		return show_obj.json();
 	})
 	.then(function(obj) {
 		obj.forEach(function(item,index) {
-				var avatar = document.createElement("img");
-				avatar.setAttribute("src", obj[index].avatar_url);
-				avatar.setAttribute("id", "f-icon");
-				avatar.setAttribute("width", "6%");
-				avatar.setAttribute("height", "6%");
-				avatar.setAttribute("alt", "Avatar");
-				avatar.setAttribute("hspace", "1%");
-				document.getElementById("following_avatar").appendChild(avatar);
+			var span = document.createElement("span");
+			span.innerHTML = '<a href="http://www.github.com/'+ obj[index].login + '" target="_blank">' + '<img src="' + obj[index].avatar_url + '" id="f-icon" width=6% height=6% alt="follower avatar" hspace=1%/>';
+			document.getElementById("following_avatar").appendChild(span);
 		});
 	})
 }
